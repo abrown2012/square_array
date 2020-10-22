4f0f7e404a6f41f14166422a21f4327ec4048550
Agnieszka Brown <abrown2012@icloud.com>
2020-10-22 19:48:44 -0400

    commit tree edb218eddd490a3272509363ced94c4410a26b83

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+
+
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |number|
+    number = number * number 
+    new_array.push(number)
+
+  end 
+  return new_array
 end
